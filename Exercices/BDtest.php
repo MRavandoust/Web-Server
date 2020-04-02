998785796f6a020e693cc5d76dd03a11f4222642
Masoud RAVANDOUST <akam_50@yahoo.com>
2020-04-02 12:14:07 +0200

    55

--- Exercices/BDtest.php
+++ Exercices/BDtest.php
@@ -1,6 +1,6 @@
 <?php 
 $connect = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$reponse = $connect->query('SELECT * FROM visite');
+$reponse = $connect->query('SELECT * FROM utilisateurs');
 while($donnee = $reponse-> fetch())
 {
     echo '<p>' . $donnee['email'] . '</p>';
